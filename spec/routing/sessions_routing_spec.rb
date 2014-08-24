8c5b85b42523f0940743b126a050e232ecf7d700
danlaffan <dan.laffan@mac.com>
2014-08-24 17:46:46 +0100

    Fixed a weird routing test

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe SessionsController do
   describe 'routing' do
 
-    # get 'auth/:provider/callback', to: 'sessions#create'
-    # get 'auth/failure', to: redirect('/')
-    # post 'signout', to: 'sessions#destroy', as: 'signout'
-
     it 'routes to create' do
-      get('auth/friend_face/callback').should route_to('sessions#create', provider: 'friend_face')
-    end
-
-    it 'routes to root on failure' do
-      get('auth/failure').should route_to('home#index')
+      get('/auth/friend_face/callback').should route_to('sessions#create', provider: 'friend_face')
     end
 
     it 'routes to signout' do
-      post('signout').should route_to('sessions#destroy')
+      post('/signout').should route_to('sessions#destroy')
     end
 
   end
+
+  describe 'redirection', type: :request do
+    it 'routes to root on failure' do
+      get('/auth/failure')
+      response.should redirect_to(root_url)
+    end
+  end
+
 end
