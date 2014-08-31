791469ae716af07bae69aa04df5688481199910d
danlaffan <dan.laffan@mac.com>
2014-08-31 17:40:50 +0100

    Fine tuned sessionsController to work with Rspec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   protected
 
   def allowed_params
-    ActionController::Parameters.new(env['omniauth.auth']).permit(:provider, :uid, info: [:email, :name, :first_name, :last_name, :image, :token, :verified, urls: [:Facebook]], credentials: [:token, :expires_at, :expires], extra: [raw_info: [:id, :email, :first_name, :gender, :last_name, :link, :locale, :name, :timezone, :updated_time, :verified]])
+    ActionController::Parameters.new(request.env['omniauth.auth']).permit(:provider, :uid, info: [:email, :name, :first_name, :last_name, :image, :token, :verified, urls: [:Facebook]], credentials: [:token, :expires_at, :expires], extra: [raw_info: [:id, :email, :first_name, :gender, :last_name, :link, :locale, :name, :timezone, :updated_time, :verified]])
   end
 
 end
