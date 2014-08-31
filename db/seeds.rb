b10468b031c59298907fb17806a6dcf4f8d52a6d
danlaffan <dan.laffan@mac.com>
2014-08-31 19:40:18 +0100

    Added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+QuizEvent.where(id: 1).first_or_create(
+        name: 'School Fundraiser', 
+        description: 'To help raise funds for new computers for our school', 
+        time_zone: 'Dublin', 
+        starts_at: '2014-08-31 18:09:07', 
+        venue_id: 1, 
+        created_by: 1, 
+        updated_by: nil, 
+        event_guid: 'WKZEDcEP', 
+        sponsor_logo_url: nil, 
+        logo_file_name: nil, 
+        logo_content_type: nil, 
+        logo_file_size: nil, 
+        logo_updated_at: nil)
+
+Venue.where(id: 1).first_or_create(
+        public_title: 'Oscars of Smithfield',
+        address: 'Smithfield Square, Dublin 7, Ireland', 
+        latitude: 53.5, 
+        longitude: -6.1, 
+        created_by: 1, 
+        updated_by: nil, 
+        publicly_visible: true, 
+        approved_by: 1, 
+        time_zone: 'Dublin', 
+        logo_file_name: nil, 
+        logo_content_type: nil, 
+        logo_file_size: nil, 
+        logo_updated_at: nil)
+
