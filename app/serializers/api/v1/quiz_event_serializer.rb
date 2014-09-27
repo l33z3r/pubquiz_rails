b9414ad2169cefbda33c53a2998ed14acbd3760b
danlaffan <dan.laffan@mac.com>
2014-09-27 17:08:59 +0100

    Further work on the api serializers

--- app/serializers/api/v1/quiz_event_serializer.rb
+++ app/serializers/api/v1/quiz_event_serializer.rb
@@ -1,7 +1,12 @@
 class Api::V1::QuizEventSerializer < ActiveModel::Serializer
+
+  embed :ids, include: true
+
   attributes :id, :name, :description, :time_zone, :starts_at, :logo_url,
              :open_to_the_public, :in_dst, :venue_id,
-             :quiz_rounds, :quiz_round_questions
+             :quiz_round_questions
+
+  has_many :quiz_rounds
 
   def starts_at
     object.starts_at.to_i
@@ -10,16 +15,6 @@ class Api::V1::QuizEventSerializer < ActiveModel::Serializer
   def logo_url
     # logo of the sponsor
     object.logo.try(:url)
-  end
-
-  def quiz_rounds # todo
-    [{id: 123, sorting_order: 10, name: 'Lorem ipsum',
-      quiz_round_question_ids: [1, 2, 3]},
-     {id: 456, sorting_order: 20, name: 'Lorem ipsum',
-      quiz_round_question_ids: [4, 5, 6]},
-     {id: 789, sorting_order: 30, name: 'Lorem ipsum',
-      quiz_round_question_ids: [7, 8, 9]}
-    ]
   end
 
   def quiz_round_questions # todo
@@ -32,7 +27,7 @@ class Api::V1::QuizEventSerializer < ActiveModel::Serializer
              {visible_text: 'Filipe Masse', id: 123},
              {visible_text: 'Foghorn Leghorn', id: 124},
              {visible_text: 'Chilton Pike', id: 125},
-             {visible_text: 'Michael Schumaker', id: 126},
+             {visible_text: 'Michael Schumacher', id: 126},
      ],
      time_limit_seconds: 10,
      points_available: 20
